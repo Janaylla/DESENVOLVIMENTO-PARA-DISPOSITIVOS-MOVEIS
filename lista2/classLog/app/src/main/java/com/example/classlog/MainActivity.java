d90289eaa8f6c648dc987f4bfad1e3438c2e33ff
Janaylla <janayllaf@gmail.com>
2023-08-09 11:25:12 -0300

    ) Desenvolva uma aplicação conta clicks, que conta o número de clicks que o usuário realizou em um botão

---
+++ lista2/classLog/app/src/main/java/com/example/classlog/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.classlog;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText num1EditText, num2EditText;
+    private Button calculateButton;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1EditText = findViewById(R.id.num1EditText);
+        num2EditText = findViewById(R.id.num2EditText);
+        calculateButton = findViewById(R.id.calculateButton);
+        resultTextView = findViewById(R.id.resultTextView);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateSum();
+            }
+        });
+    }
+
+    private void calculateSum() {
+        String num1Str = num1EditText.getText().toString();
+        String num2Str = num2EditText.getText().toString();
+
+        if (!num1Str.isEmpty() && !num2Str.isEmpty()) {
+            double num1 = Double.parseDouble(num1Str);
+            double num2 = Double.parseDouble(num2Str);
+
+            double sum = num1 + num2;
+
+            resultTextView.setText("Resultado: " + sum);
+            Log.i("Soma: ", num1 + " + " + num2 + " = " + sum);
+        } else {
+            resultTextView.setText("Preencha ambos os números");
+        }
+    }
+}
